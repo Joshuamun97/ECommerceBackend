330d763af5e21531c5b350345f8e164e189da248
Joshua_Muniz <joshua.mun97@gmail.com>
2023-02-25 19:21:27 -0700

    Completed routes and updated server

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     const ProductData = Product.findAll({
       include: [{ model: Category }, { model: Tag }]
     });
-    res.status(200).json(productData);
+    res.status(200).json(ProductData);
   } catch (err) {
     res.status(500).json(err);
   }
