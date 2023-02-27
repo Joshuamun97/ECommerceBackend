abed235dadb871de7df414a6ff2a9d8874138732
Joshua_Muniz <joshua.mun97@gmail.com>
2023-02-27 16:12:59 -0700

    Update

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,19 +2,19 @@ const { Category } = require('../models');
 
 const categoryData = [
   {
-    category_name: 'Shirts',
+    category_names: 'Shirts',
   },
   {
-    category_name: 'Shorts',
+    category_names: 'Shorts',
   },
   {
-    category_name: 'Music',
+    category_names: 'Music',
   },
   {
-    category_name: 'Hats',
+    category_names: 'Hats',
   },
   {
-    category_name: 'Shoes',
+    category_names: 'Shoes',
   },
 ];
 
